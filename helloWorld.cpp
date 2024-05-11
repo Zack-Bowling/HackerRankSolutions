8d2277a1bbca2151a56de39fa4da2c4744384ac1
Zack Bowling <135551008+zbowling25@users.noreply.github.com>
2024-05-10 22:37:27 -0500

    Create helloWorld.cpp

---
+++ helloWorld.cpp
@@ -0,0 +1,11 @@
+/*
+I am only including this to satisfy my completionist attitude, this is very obviously a simple C++ program that I learned to do like 10 years ago. Hello, World!
+*/
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
