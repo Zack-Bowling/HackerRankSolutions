b6bbef229465bc7e8cb1232a2c112c8117672f72
Zack Bowling <135551008+Zack-Bowling@users.noreply.github.com>
2025-04-18 09:37:22 -0500

    Create minMaxSum.cpp

---
+++ minMaxSum.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ miniMaxSum function takes an array of size 5 and calculates the minimum and maximum sum of the array using only 4 out of 5 integers in the array.
+I choose to sort the array in ascending order and then calculate the max and min in a brute force - style
+ */
+
+void miniMaxSum(vector<int> arr) {
+    sort(arr.begin(),arr.end()); 
+    long long int max = 0;
+    long long int min = 0;
+    
+    max = (long long) arr[1] + arr[2] + arr[3] + arr[4];
+    min = (long long) arr[0] + arr[1] + arr[2] + arr[3];
+    
+    cout << min << " " << max;
+}
+
+int main()
+{
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(5);
+
+    for (int i = 0; i < 5; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    miniMaxSum(arr);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
